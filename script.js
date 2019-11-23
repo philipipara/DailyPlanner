5270e0fbffb4229a9f3add9c4ada23606a3c5434
philipipara <54330369+philipipara@users.noreply.github.com>
2019-11-22 19:14:43 -0800

    close to working time stamp, i think

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-function currentDate(current) {
-    var today = new Date();
-    date = moment().format("LL");
-    
-    $("#currentDay").html(date);
- }
 
+var save = document.querySelector("#saveBtn");
+
+
+$("#saveBtn").click(function(){
+    document.getElementById("#task").html("textarea");
+    localStorage.setItem("textarea");
+
+    
+});
+
+
+$("#saveBtn").click(function(){
+    var userInput = $("#task").val();
+    document.getElementById("#task").html("textarea");
+    localStorage.setItem("#task");
+
+});
+
+var currentDate = moment().format("MMMM Do YYYY");
+console.log(currentDate);
+var hour = moment().hour();
+var moment = moment();
+console.log(moment);
+console.log(hour);
+
+$("#currentDay").text(currentDate);
