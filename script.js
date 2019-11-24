15114ebb4745b2c7e1c790aaf6263c4dc7e425a0
philipipara <54330369+philipipara@users.noreply.github.com>
2019-11-23 20:09:12 -0800

    local storage working

--- script.js
+++ script.js
@@ -1,27 +1,71 @@
+$(document).ready(function(){
+  
+  var nowMoment = moment();
+  var displayMoment = doucment.getElementbyId("#currentDay");
+    displayMoment.innerHTML= nowMoment;
 
-var save = document.querySelector("#saveBtn");
+  var nineSlot = localStorage.getItem("nineSlot");
+    $("#9A").val(nineSlot);
+
+  var tenSlot = localStorage.getItem("tenSlot");
+    $("#10A").val(tenSlot);
+
+  var elevenSlot = localStorage.getItem("elevenSlot");
+    $("#11A").val(elevenSlot);
+
+  var twelveSlot = localStorage.getItem("noonSlot");
+    $("#12p").val(twelveSlot);
+
+  var oneSlot = localStorage.getItem("oneSlot");
+    $("#1P").val(oneSlot);
+
+  var twoSlot = localStorage.getItem("twoSlot");
+    $("#2P").val(twoSlot);
+
+  var threeSlot = localStorage.getItem("threeSlot");
+    $("#3P").val(threeSlot);
+
+  var fourSlot = localStorage.getItem("fourSlot");
+    $("#4P").val(fourSlot);
+
+  var fiveSlot = localStorage.getItem("fiveSlot");
+    $("#5P").val(fiveSlot);
 
 
-$("#saveBtn").click(function(){
-    document.getElementById("#task").html("textarea");
-    localStorage.setItem("textarea");
+//save to local storage
 
-    
+$(".btn").on("click", function(e){
+   e.preventDefault();
+   
+
+var nineAm = $("#9A").val();
+var tenAm = $("#10A").val();
+var elevenAm= $("#11A").val();
+var noonPm = $("#12P").val();
+var onePm = $("#1P").val();
+var twoPm= $("#2P").val();
+var threePm = $("#3P").val();
+var fourPm= $("#4P").val();
+var fivePm = $("#5P").val();
+
+localStorage.setItem("nineSlot", nineAm);
+localStorage.setItem("tenSlot", tenAm);
+localStorage.setItem("elevenSlot", elevenAm);
+localStorage.setItem("noonSlot", noonPm);
+localStorage.setItem("oneSlot", onePm);
+localStorage.setItem("twoSlot", twoPm);
+localStorage.setItem("threeSlot", threePm);
+localStorage.setItem("fourSlot", fourPm);
+localStorage.setItem("fiveSlot", fivePm);
+
 });
 
 
-$("#saveBtn").click(function(){
-    var userInput = $("#task").val();
-    document.getElementById("#task").html("textarea");
-    localStorage.setItem("#task");
+
+
+
+
+
+
 
 });
-
-var currentDate = moment().format("MMMM Do YYYY");
-console.log(currentDate);
-var hour = moment().hour();
-var moment = moment();
-console.log(moment);
-console.log(hour);
-
-$("#currentDay").text(currentDate);
